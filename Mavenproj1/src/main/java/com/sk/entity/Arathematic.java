187cbe947aa5506b2fe9840dac16e0f80c5d5521
sai kalyan <sai kalyan@LAPTOP-K7R95M54>
2024-07-12 14:48:10 +0530

    TL- created structured

---
+++ Mavenproj1/src/main/java/com/sk/entity/Arathematic.java
@@ -0,0 +1,10 @@
+package com.sk.entity;
+
+public class Arathematic 
+{
+	public int sum(int a,int b)
+	{
+		return a+b;
+	}
+
+}
---
+++ Mavenproj1/src/test/java/com/sk/entity/AppTest.java
@@ -0,0 +1,18 @@
+package com.sk.entity;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
